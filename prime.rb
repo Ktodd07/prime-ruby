3b9a0050354f2cdccc90cbe275448461cff67301
Kendan <ktoddevart@outlook.com>
2020-11-16 13:07:20 -0500

    commit tree 27ca1a68a10a0f9e91748cf379e98fde5da67f9a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+    start = 2
+    if num > 1
+        range = (start..num - 1).to_a
+        range.none? do |divisor|
+            num % divisor == 0
+        end
+    else
+        return false
+    end
+end
